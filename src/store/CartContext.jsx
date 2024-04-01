7a9ff9e49fa0cb3b9c3d4bce2d573f7c957f30be
GiorgiGvasalia <ggvasalia4@gmail.com>
2024-04-01 18:00:35 +0400

    removing meal items from cart

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -33,15 +33,53 @@ function cartReducer(state, action) {
     return { ...state, items: updatedItems };
   }
 
- 
+  if(action.type === 'REMOVE_ITEM'){
+    const existingItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    
+    const existingCartItem = state.items[existingItemIndex]
+    const updatedItems = [...state.items]
+    if( existingCartItem.quantity === 1){
+      updatedItems.splice(existingItemIndex, 1)
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity -1
+      }
+      updatedItems[existingItemIndex] = updatedItem
+    }
+    return { ...state, items: updatedItems}
+  }
 
   return state
 }
 
 export function CartContextProvider({ children }) {
-  useReducer(cartReducer, { items: [] });
+  const [ cart, dispatchCartAction ] = useReducer(cartReducer, { items: [] });
 
-  return <CartContext.Provider>{children}</CartContext.Provider>;
+  
+  function addItem(item){
+    dispatchCartAction({
+      type: 'ADD_ITEM',
+      item
+    })
+  }
+  
+  function removeItem(id){
+    dispatchCartAction({
+      type: 'REMOVE_ITEM',
+      id
+    })
+  }
+  
+  const cartContext = {
+    items: cart.items,
+    addItem,
+    removeItem
+  }
+
+  return <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>;
 }
 
 export default CartContext;
